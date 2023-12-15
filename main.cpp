bd927f4eb96bbc505e4c02c0a9fe88fd27a63737
Pon4ik123 <taranenkozahar@gmail.com>
2023-12-15 01:45:32 +0100

    done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
+// ALGO2 IS1 221A LAB05
+// Zakhar Taranenko
+// tz53770@zut.edu.pl
+
 #include <iostream>
 #include <math.h>
 
 using namespace std;
 
-template<typename T>
+template <typename T>
 class MaxHeap {
 public:
-    T *array;
+    T* array;
     int size;
     int capacity;
 
@@ -22,9 +26,7 @@ public:
     }
 
     int parent(T number) { return (number - 1) / 2; }
-
     int left(T number) { return (2 * number + 1); }
-
     int right(T number) { return (2 * number + 2); }
 
     void swap(T &x, T &y) {
@@ -37,9 +39,10 @@ public:
         if (capacity == 0) {
             capacity++;
             array = new T[capacity];
-        } else if (size == capacity) {
+        }
+        else if (size == capacity) {
             capacity *= 2;
-            T *temp = new T[capacity];
+            T* temp = new T[capacity];
             for (int i = 0; i < size; i++) {
                 temp[i] = array[i];
             }
@@ -61,10 +64,10 @@ public:
         int r = right(index);
         int max = index;
 
-        if (r < size && array[r] > array[index]) {
+        if (r<size && array[r]>array[max]) {
             max = r;
         }
-        if (l < size && array[l] > array[index]) {
+        if (l < size && array[l] > array[max]) {
             max = l;
         }
         if (max != index) {
@@ -78,7 +81,7 @@ public:
             abort();
         }
         T root = array[0];
-        array[0] = array[size - 1];
+        array[0] = array[size-1];
         size--;
         heapify(0);
         return root;
@@ -104,14 +107,14 @@ public:
 };
 
 int main() {
-    MaxHeap<int> *m = new MaxHeap<int>();
+    MaxHeap<int>* m = new MaxHeap<int>();
     const int MAX_ORDER = 7;
 
     for (int o = 1; o <= MAX_ORDER; o++) {
         const int n = pow(10, o);
         clock_t t1 = clock();
         for (int i = 0; i < n; i++) {
-            m->add(rand() % 100);
+            m->add(rand()%100);
         }
         clock_t t2 = clock();
         double time = static_cast<double>(t2 - t1) / CLOCKS_PER_SEC;
@@ -119,7 +122,7 @@ int main() {
 
         clock_t t11 = clock();
         for (int i = 0; i < n; i++) {
-            m->deletRoot();
+            cout << m->deletRoot() << endl;
         }
         clock_t t22 = clock();
 
